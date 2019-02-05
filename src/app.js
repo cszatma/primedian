6a61d9373d2d04d90fd9b3959b87f94b504ac368
Christopher Szatmary <cs@christopherszatmary.com>
2019-02-04 23:08:58 -0500

    Added primeRoutes to app, in production mode app will server static assets

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
-const helloRoutes = require('./routes/hello');
+const primeRoutes = require('./routes/prime');
 
 const app = express();
 
-app.use('/', helloRoutes);
+app.use('/', primeRoutes);
+
+// If in production mode server the client files
+// This is not necessary in development as the webpack dev server provided by create-react-app will be running
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  const clientBuildDir = '../client/build';
+
+  // Express will server up production assets from the client
+  app.use(express.static(path.resolve(__dirname, clientBuildDir)));
+
+  // Express will serve up the index.html file if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, clientBuildDir, 'index.html'));
+  });
+}
 
 module.exports = app;
