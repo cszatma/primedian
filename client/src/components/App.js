e992a1178dd0ce1e54accc3e43a2dbd3b98b2497
Christopher Szatmary <cs@christopherszatmary.com>
2019-02-07 00:07:40 -0500

    Removed unused styles for App, added styles for container, removed extra div tag in App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,20 +7,18 @@ import MedianPrimeGenerator from './MedianPrimeGenerator';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Container className={styles.AppContainer}>
-          <Row className="justify-content-center">
-            <Col lg="8" md="6" sm="8" xs="8">
-              <header className="my-3">
-                <Container>
-                  <h1>PriMedian</h1>
-                </Container>
-              </header>
-              <MedianPrimeGenerator />
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container className={styles.AppContainer}>
+        <Row className="justify-content-center">
+          <Col lg="8" md="6" sm="8" xs="8">
+            <header className="my-3">
+              <Container>
+                <h1>PriMedian</h1>
+              </Container>
+            </header>
+            <MedianPrimeGenerator />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
